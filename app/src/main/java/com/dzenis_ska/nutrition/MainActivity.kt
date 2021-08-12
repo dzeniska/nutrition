b174ba589c0652c18a12dc4fc0dc2d3950f586c9
dzeniska <dzenbuddha@yandex.ru>
2021-08-12 11:29:06 +0300

    test1

--- app/src/main/java/com/dzenis_ska/nutrition/MainActivity.kt
+++ app/src/main/java/com/dzenis_ska/nutrition/MainActivity.kt
@@ -8,7 +8,6 @@ import android.view.animation.AnimationUtils
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.broooapps.graphview.CurveGraphConfig
 import com.broooapps.graphview.CurveGraphView
@@ -90,10 +89,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun onCLick(): View.OnClickListener? {
         return View.OnClickListener { view ->
-//            Log.d("!!!", "${job}")
+
             if (job == null) {
                 job = CoroutineScope(Dispatchers.Main).launch {
-//                    for(item in 0..listViewButtons.size)
                     val index = listViewButtons.indexOf(view)
                     listViewButtons.forEach {
                         if (it != view) {
@@ -120,7 +118,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun btnAddKcal(index: Int): View.OnClickListener? {
+    private fun btnAddKcal(index: Int): View.OnClickListener {
         return View.OnClickListener {
             dialogHelper.createDialog(index)
         }
@@ -130,9 +128,6 @@ class MainActivity : AppCompatActivity() {
 
     private suspend fun elevateDown(view: View, viewCl: View) = withContext(Dispatchers.IO) {
         var el = view.elevation
-//        var elev = (el * 10).toInt()
-        Log.d("!!!elevDown", "${el}")
-        Log.d("!!!elevDown", "${view.translationY}")
 
         for (i in 20 downTo 1) {
             el -= 2.0f
@@ -140,86 +135,38 @@ class MainActivity : AppCompatActivity() {
             view.translationY = -el/2/*ev.toFloat() / 20*/
             viewCl.elevation = el.plus(1.0f)/*ev.toFloat() / 10*/
             viewCl.translationY = -el/2/*ev.toFloat() / 20*/
-            Log.d("!!!elevDd2", "${ -el}")
-            Log.d("!!!elevDd2", "${ el/2}")
-//            elev -= 10
             delay(10)
         }
-        Log.d("!!!elevDown", "${ view.elevation}")
-        Log.d("!!!elevDown", "${ view.translationY}")
-        Log.d("!!!elevDown", "${ viewCl.elevation}")
-        Log.d("!!!elevDown", "${ viewCl.translationY}")
+
         view.elevation = 0.0f
         view.translationY = 0.0f
         viewCl.elevation = 0.0f
         viewCl.translationY = 0.0f
 
 
-        //тут косяк с alpha значением
-
-//        var alpha = 1.0f
-//        for (i in 5 downTo 0) {
-//            view.alpha = alpha
-//            alpha -= 0.1f
-//            Log.d("!!!alpha", "${alpha}")
-//            delay(4)
-//        }
-//            Log.d("!!!alpha", "${alpha}")
-
         val img = view as ImageButton
         img.background = resources.getDrawable(R.drawable.img_btn_back_normal)
-
-//        for (i in 10 downTo 1) {
-//            view.alpha = alpha
-//            alpha += 0.02f
-//            delay(2)
-//        }
-//        Log.d("!!!alpha", "${alpha}")
-////            Log.d("!!!", "${alpha}")
-//        view.alpha = 1.0f
     }
 
     private suspend fun elevateUp(view: View, viewCl: View) = withContext(Dispatchers.IO) {
         var el = view.elevation
 
-//        var elev = (el * 10).toInt()
-        Log.d("!!!elevUp", "${ view.elevation}")
-        Log.d("!!!elevUp", "${ view.translationY}")
-
         view.background =
             resources.getDrawable(R.drawable.img_btn_back_pressed)
-//        var alpha = 0.8f
         for (i in 0..40) {
-//            Log.d("!!!eleV", "${elev.toFloat()/10}")
-            viewCl.elevation = /*(*/el.plus(1.0f)/*ev.toFloat() / 10).plus(2)*/
-            view.elevation = el/*ev.toFloat() / 10*/
-            viewCl.translationY = -el/2/*ev.toFloat() / 20*/
-            view.translationY = -el/2/*ev.toFloat() / 20*/
-//            Log.d("!!!eleC", "${elev.toFloat()/10}")
+            viewCl.elevation = el.plus(1.0f)
+            view.elevation = el
+            viewCl.translationY = -el/2
+            view.translationY = -el/2
 
-
-            Log.d("!!!elevUd2", "${ -el/2}")
-            Log.d("!!!elevUd2", "${ el}")
-//            alpha += 0.005f
-//            view.alpha = alpha
-//            elev += 10
             el += 1.0f
             delay(10)
         }
-        Log.d("!!!elevUp", "${ view.elevation}")
-        Log.d("!!!elevUp", "${ view.translationY}")
-
-
     }
 
     fun showResult(index: Int, text: String) {
 
-
-        Log.d("!!!y", "${rootElement!!.tvGr1.y}")
-//        rootElement!!.tvGr1.y = 500.0f
-
         rootElement!!.apply {
-//            tvInputKcal1.text = text
             val view = listViewTvKcal[index] as TextView
             val viewGr = listViewTvGr[index] as TextView
             view.text = text
@@ -231,9 +178,6 @@ class MainActivity : AppCompatActivity() {
                 val sum = el.text.toString().toInt()
                 eating += sum
             }
-
-
-            //можно и viewModel сделать
 
             val listHeight = arrayListOf<Int>()
 
@@ -258,7 +202,6 @@ class MainActivity : AppCompatActivity() {
 
                     heightListNormal.add(h)
                     val hInt = h.toInt()
-                    Log.d("!!!h", "$hInt")
                     cs.clone(rootElement!!.constrLayoutGraph)
                     cs.connect(listIntTvGr[indexL], ConstraintSet.BOTTOM, R.id.constrLayoutGraph, ConstraintSet.BOTTOM, (hInt*0.60f).toInt())
 //        cs.setVerticalBias(R.id.tvGr1, 0.5f)
@@ -270,7 +213,6 @@ class MainActivity : AppCompatActivity() {
                 var indexL = 0
                 listHeight.forEach {
                     heightListNormal.add(it.toFloat())
-                    Log.d("!!!h", "$it")
                     cs.clone(rootElement!!.constrLayoutGraph)
                     cs.connect(listIntTvGr[indexL], ConstraintSet.BOTTOM, R.id.constrLayoutGraph, ConstraintSet.BOTTOM, (it*0.60f).toInt())
 //        cs.setVerticalBias(R.id.tvGr1, 0.5f)
@@ -281,12 +223,6 @@ class MainActivity : AppCompatActivity() {
             }
 //            сделать текст bias
 
-//            var kcalGraph = text.toInt()
-//            if(kcalGraph > MAX_HEIGHT){
-//                kcalGraph = MAX_HEIGHT
-//
-//            }
-//            pointMap(kcalGraph, 300, 890)
 
             val viewClock = listViewTvCock[index] as TextView
             val dataFormat = SimpleDateFormat("h:mm a")
@@ -295,7 +231,6 @@ class MainActivity : AppCompatActivity() {
             tvEatingRes.text = "${eating} kcal"
             val burnt = tvBurntRes.text.toString()
             val total = eating - burnt.substringBefore(" ").toInt()
-//            Log.d("!!!", "${burnt.substringBefore(" ").toInt()}")
             tvTotalRes.text = "${total}"
 
         }
@@ -321,9 +256,6 @@ class MainActivity : AppCompatActivity() {
 //                .setAnimationDuration(2000)
                 .build()
         )
-
-
-//        pointMap(50, 60, 30)
     }
 
     private fun pointMap(v1: Int, v2: Int, v3: Int) {
@@ -338,63 +270,15 @@ class MainActivity : AppCompatActivity() {
             addPoint(7, 0)
         }
 
-//        val listGrapf = arrayListOf<GraphData>()
-//        for(i in -7..7){
-//            Log.d("!!!", "${i}")
-//            listGrapf.add(pointNextMap(i, v1.plus(i), v2.plus(i), v3.plus(i)))
-//        }
-
         val gd: GraphData = GraphData.builder(this)
             .setPointMap(pointMap)
             .setGraphStroke(R.color.bir)
             .setPointColor(R.color.black)
             .setPointRadius(5)
             .build()
-//        listGrapf.add(gd)
-
 
         curveGraphView!!.setData(7, 1000, gd)
-
-//        curveGraphView!!.setData(7, 1000,
-//            listGrapf[0],
-//            listGrapf[1],
-//            listGrapf[2],
-//            listGrapf[3],
-//            listGrapf[4],
-//            listGrapf[5],
-//            listGrapf[6],
-//            listGrapf[7],
-//            listGrapf[8],
-//            listGrapf[9],
-//            listGrapf[10],
-//            listGrapf[11],
-//            listGrapf[12],
-//            listGrapf[13],
-//            listGrapf[14],
-//            listGrapf[15]
-//        )
     }
-
-//    private fun pointNextMap(v0: Int, v1: Int, v2: Int, v3: Int): GraphData {
-//        val pointMap2 = PointMap()
-//        pointMap2.apply {
-//            addPoint(0, v0)
-//            addPoint(2, v1)
-//            addPoint(3, v0)
-//            addPoint(4, v2)
-//            addPoint(5, v0)
-//            addPoint(6, v3)
-//            addPoint(7, v0)
-//        }
-//        val gd2: GraphData = GraphData.builder(this)
-//            .setPointMap(pointMap2)
-//            .setGraphStroke(R.color.bir)
-//            .setPointColor(R.color.invisible)
-//            .setPointRadius(0)
-//            .build()
-//        return gd2
-//
-//    }
 
     companion object {
         const val MAX_HEIGHT = 500
